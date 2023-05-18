cdff640fb16b2d3d2500bad3e5ba8958a8a18491
Desmondotutu <dwangki2@gmail.com>
2023-05-18 19:08:07 +0100

    another update

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ data "aws_ami" "latest_ubuntu" {
   }
 }
 
-module "aws_vpc" {
+module "kubernetes_vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "3.19.0"
-  name            = var.vpc_name
+  name            = kubernetes_vpc
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
@@ -35,22 +35,10 @@ module "aws_vpc" {
   }
 }
 
-resource "aws_instance" "app_server" {
-  ami                         = data.aws_ami.latest_ubuntu.id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [aws_security_group.jenkins-SG.id]
-  subnet_id                   = module.aws_vpc.public_subnets[0]
-  user_data                   = file("app-server-script.sh")
-
-  tags = {
-    Name = var.instance_name
-  }
-}
 resource "aws_security_group" "jenkins-SG" {
   name        = "jenkins-security-group"
   description = "Jenkins security group"
+   vpc_id = module.kubernetes_vpc.vpc_id
 
   ingress {
     from_port   = 8080
@@ -64,5 +52,24 @@ resource "aws_security_group" "jenkins-SG" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.latest_ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.jenkins-SG.id]
+  subnet_id                   = module.kubernetes_vpc.public_subnets[0]
+  user_data                   = file("app-server-script.sh")
+
+  tags = {
+    Name = var.instance_name
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,12 +16,6 @@ variable "key_name" {
   default     = "Jenkins-server"
 }
 
-variable "vpc_name" {
-  description = "Name of VPC"
-  type        = string
-  default     = "kubernetes-vpc"
-}
-
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
@@ -43,4 +37,11 @@ variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for the public subnets"
   type        = list(string)
   default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "env_prefix" {
+  description = "environment prefix"
+  type        = string
+  default     = "dev"
+  
 }
